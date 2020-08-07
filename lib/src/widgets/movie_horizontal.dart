95b49ec47b93d090fa7ac4182a004a1f31cf9209
unknown <troy.esname@gmail.com>
2020-08-07 16:31:44 -0500

    creacion de pagina de detalle

--- lib/src/widgets/movie_horizontal.dart
+++ lib/src/widgets/movie_horizontal.dart
@@ -37,7 +37,7 @@ class MovieHorizontal extends StatelessWidget {
   }
 
   Widget _tarjeta(BuildContext context, Pelicula pelicula) {
-    return Container(
+    final pelicula_tarjeta = Container(
       margin: EdgeInsets.only(right: 15.0),
       child: Column(
         children: <Widget>[
@@ -58,6 +58,13 @@ class MovieHorizontal extends StatelessWidget {
           ),
         ],
       ),
+    );
+
+    return GestureDetector(
+      child: pelicula_tarjeta,
+      onTap: () {
+        print('id de la pelicula ${pelicula.title}');
+      },
     );
   }
 
